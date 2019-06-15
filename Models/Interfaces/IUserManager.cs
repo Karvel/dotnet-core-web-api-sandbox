245b37b3dd792b249964418bef3f93273f513e95
Elanna Grossman <elanna.grossman@gmail.com>
2019-06-14 17:49:57 -0700

    feat(Startup, Interfaces): add `IUserManager`

---
+++ Models/Interfaces/IUserManager.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+using Sandbox.Models.User;
+
+namespace Sandbox.Models.Interfaces
+{
+	public interface IUserManager
+	{
+		IEnumerable<UserEntity> GetUsers();
+		UserEntity GetUser(int id);
+		void AddUser(UserEntity entity);
+		void UpdateUser(UserEntity user, UserEntity entity);
+		void DeleteUser(UserEntity user);
+	}
+}
